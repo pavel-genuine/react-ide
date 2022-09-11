b78a81ccce9faaa1e5d2b1a98060a86d7351b976
pavel.genuine <pavel.play@gmail.com>
2022-09-12 00:48:03 +0600

    fixing webpack entry path

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
     "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
   } 
 },
-  entry: './index.js',
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
